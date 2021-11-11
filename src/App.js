16c610af6017f9fc5d15242277f89e3ad61b6276
Natacha7 <jennynatacha@misena.edu.co>
2021-11-10 19:53:39 -0500

    Se realiza Home

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
-import{Home} from './Home/Home';
-import { Landing } from './Landing/Landing';
-import { Register } from './Register/Register';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Home/Home'
+import { Landing } from './Landing/Landing'
+import { Register } from './Register/Register'
 
 function App() {
   return (
     <div className="App">
-        <Routes>
+      <Routes>
         <Route path="home" element={<Home />} />
         <Route path="/" element={<Landing />} />
         <Route path="register" element={<Register />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,17 +1,62 @@
-import React from "react";
-import { NavBar } from '../NavBar/NavBar';
+import React from 'react'
+import { NavBar } from '../NavBar/NavBar'
+//import { Container, Row } from 'reactstrap'
+import { Card, CardBody, CardImg, CardTitle, CardGroup } from 'reactstrap'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const history = useNavigate()
+  const onClickManosypies = () => {
+    history('/home')
+  }
+  const onClickGaleria = () => {
+    history('/home')
+  }
+  const onClickAgenda = () => {
+    history('/home')
+  }
   return (
     <div>
-      <NavBar/>
-      <p>
-        Bienvenido a Agendy
-      </p>
+      <NavBar />
+      <CardGroup>
+        <Card>
+          <CardImg
+            alt="Card image cap"
+            src="./Group.jpg"
+            top
+            width="100%"
+            onClick={onClickManosypies}
+          />
+          <CardBody>
+            <CardTitle tag="h5">MANOS Y PIES</CardTitle>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            alt="Card image cap"
+            src="./Group.jpg"
+            top
+            width="100%"
+            onClick={onClickGaleria}
+          />
+          <CardBody>
+            <CardTitle tag="h5">GALERIA</CardTitle>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            alt="Card image cap"
+            src="./Group 46.jpg"
+            top
+            width="100%"
+            onClick={onClickAgenda}
+          />
+          <CardBody>
+            <CardTitle tag="h5">AGENDA</CardTitle>
+          </CardBody>
+        </Card>
+      </CardGroup>
     </div>
-
-  );
+  )
 }
-
-
-export { Home };
+export default Home
