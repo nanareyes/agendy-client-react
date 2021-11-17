7d7fcf37a95d4058ac92213f6d51941531339772
nanareyes <dianac.reyesmantilla@gmail.com>
2021-11-16 23:13:32 -0500

    Se incluyen los componentes de recuperación de contraseña y se incluyen las rutas en login para pruebas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
-import{Home} from './Home/Home';
+import{ Home } from './Home/Home';
 import { Landing } from './Landing/Landing';
 import { Register } from './Register/Register';
 import { Profile } from './Profile/Profile';
+import {ResetPassword} from './Password/ResetPassword.jsx'
+import {ForgotPassword} from './Password/ForgotPassword.jsx'
 
 function App() {
   return (
@@ -13,9 +15,11 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="register" element={<Register />} />
         <Route path="profile" element={<Profile />} />
+        <Route path="forgotpassword" element={<ForgotPassword/>} />
+        <Route path="resetpassword" element={<ResetPassword/>} />
       </Routes>
     </div>
   )
 }
 
-export default App
+export {App};
