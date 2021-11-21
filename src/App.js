9608fe4575616f4888385816925e155643b13953
subcorporeal <subcorporeal@gmail.com>
2021-11-21 17:48:07 -0500

    galleria agregada a la ruta

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './Home/Home'
-import { Landing } from './Landing/Landing'
-import { Profile } from './Profile/Profile'
-import { ResetPassword } from './Password/ResetPassword.jsx'
-import { ForgotPassword } from './Password/ForgotPassword.jsx'
-import { Appointment } from './Appointment/Appointment'
-import { Agenda } from './Agenda/Agenda'
+import {Routes, Route} from 'react-router-dom'
+import {Home} from './Home/Home'
+import {Landing} from './Landing/Landing'
+import {Profile} from './Profile/Profile'
+import {ResetPassword} from './Password/ResetPassword.jsx'
+import {ForgotPassword} from './Password/ForgotPassword.jsx'
+import {Appointment} from './Appointment/Appointment'
+import {Agenda} from './Agenda/Agenda'
+import {Cards} from './Componentes/Cards'
 
 // PrimeReact Imports
 import 'primereact/resources/themes/lara-light-indigo/theme.css'
@@ -19,20 +20,21 @@ import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
 
 function App() {
-	return (
-		<div className='App'>
-			<Routes>
-				<Route path='home' element={<Home />} />
-				<Route path='/' element={<Landing />} />
-				<Route path='register' element={<Landing />} />
-				<Route path='profile' element={<Profile />} />
-				<Route path='agenda' element={<Agenda />} />
-				<Route path='appointment' element={<Appointment />} />
-				<Route path='forgotpassword' element={<ForgotPassword />} />
-				<Route path='resetpassword' element={<ResetPassword />} />
-			</Routes>
-		</div>
-	)
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="register" element={<Landing />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="galeria" element={<Cards />} />
+        <Route path="agenda" element={<Agenda />} />
+        <Route path="appointment" element={<Appointment />} />
+        <Route path="forgotpassword" element={<ForgotPassword />} />
+        <Route path="resetpassword" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  )
 }
 
-export { App }
+export {App}
--- src/Componentes/Cards.js
+++ src/Componentes/Cards.js
@@ -1,43 +1,43 @@
-import React from "react";
-import Card from "./Card";
+import React from 'react'
+import Card from './Card'
 
-import image1 from "../assets/image1.jpg";
+import image1 from '../assets/image1.jpg'
 import image2 from '../assets/image2.jpg'
 import image3 from '../assets/image3.jpg'
 
 const cards = [
-{
+  {
     id: 1,
-    title: "Fazt Web",
+    title: 'Fazt Web',
     image: image1,
-    url: "https://faztweb.com",
-},
-{
+    url: 'https://faztweb.com',
+  },
+  {
     id: 2,
-    title: "Fazt Blog",
+    title: 'Fazt Blog',
     image: image2,
-    url: "https://blog.faztweb.com",
-},
-{
+    url: 'https://blog.faztweb.com',
+  },
+  {
     id: 3,
-    title: "Fazt Youtube",
+    title: 'Fazt Youtube',
     image: image3,
-    url: "https://youtube.com/fazttech",
-},
-];
+    url: 'https://youtube.com/fazttech',
+  },
+]
 
-function Cards() {
-return (
+const Cards = () => {
+  return (
     <div className="container d-flex justify-content-center align-items-center h-100">
-    <div className="row">
-        {cards.map(({ title, image, url, id }) => (
-        <div className="col-md-4" key={id}>
+      <div className="row">
+        {cards.map(({title, image, url, id}) => (
+          <div className="col-md-4" key={id}>
             <Card imageSource={image} title={title} url={url} />
-        </div>
+          </div>
         ))}
+      </div>
     </div>
-    </div>
-);
+  )
 }
 
-export default Cards;
+export {Cards}
