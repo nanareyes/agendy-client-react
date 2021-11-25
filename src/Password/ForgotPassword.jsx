055b04d52e788cce7a77f839f8facffc295eb23b
Natacha7 <jennynatacha@misena.edu.co>
2021-11-24 21:33:32 -0500

    Se envia correo desde front para recuperar contraseña

--- src/Password/ForgotPassword.jsx
+++ src/Password/ForgotPassword.jsx
@@ -1,35 +1,37 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Spinner } from 'reactstrap';
-import Swal from 'sweetalert2';
+import React, {useState} from 'react'
+import axios from 'axios'
+import {Spinner} from 'reactstrap'
+import Swal from 'sweetalert2'
 
 const ForgotPassword = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleSubmit = async (e) => {
-    e.peventDefault();
+    e.preventDefault()
 
     const userEmail = {
-      email: e.target.email.value
-    };
+      email: e.target.email.value,
+    }
 
-    setIsLoading(true);
-    await axios.post(`https://agendy-api.herokuapp.com/forgotpassword`, userEmail)
+    setIsLoading(true)
+    await axios
+      .post(`https://agendy-api.herokuapp.com/api/forgotpassword`, userEmail)
       .then((res) => {
         setIsLoading(false)
         Swal.fire({
           showConfirmButton: true,
           icon: 'success',
-          text: 'Revise su email, se le ha enviado un enlace para crear una nueva contraseña'
+          text: 'Revise su email, se le ha enviado un enlace para crear una nueva contraseña',
         })
-      }).catch((err) => {
+      })
+      .catch((err) => {
         setIsLoading(false)
         Swal.fire({
           showConfirmButton: true,
           icon: 'error',
-          text: 'Ha habido un error al intentar enviar los datos, comprueba el correo introducido o vuelva intentarlo más tarde'
+          text: 'Ha habido un error al intentar enviar los datos, comprueba el correo introducido o vuelva intentarlo más tarde',
         })
-      });
+      })
   }
 
   return (
@@ -37,23 +39,23 @@ const ForgotPassword = () => {
       <form className="mainContainer" onSubmit={handleSubmit}>
         <h3>Recuperar cuenta</h3>
         <div>Correo electrónico: </div>
-        <input type="email" name="email" placeholder="Introduce tu email" required />
+        <input
+          type="email"
+          name="email"
+          placeholder="Introduce tu email"
+          required
+        />
         <div className="divButton">
-          {isLoading
-            ?
-            <Spinner>
-              Loading...
-            </Spinner>
-            :
+          {isLoading ? (
+            <Spinner>Loading...</Spinner>
+          ) : (
             <button type="submit">Enviar</button>
-          }
-          <a href="/resetpassword">
-            {' '}resetear mi contraseña ¡
-          </a>
+          )}
+          <a href="/resetpassword"> resetear mi contraseña ¡</a>
         </div>
       </form>
     </div>
   )
 }
 
-export { ForgotPassword };
+export {ForgotPassword}
--- src/Password/ResetPassword.jsx
+++ src/Password/ResetPassword.jsx
@@ -1,74 +1,70 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
-import { Spinner } from 'reactstrap'
-import Swal from 'sweetalert2';
+import React, {useState} from 'react'
+import axios from 'axios'
+import {useNavigate, useParams} from 'react-router-dom'
+import {Spinner} from 'reactstrap'
+import Swal from 'sweetalert2'
 
 const ResetPassword = (props) => {
-  let { id } = useParams();
-  let { token } = useParams();
-  const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
+  let {id} = useParams()
+  let {token} = useParams()
+  const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
 
   const regExPassword = /.*/
 
-
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     const userPassword = {
-      password: e.target.password.value
+      password: e.target.password.value,
     }
 
-    if (!regExPassword.test(userPassword.password)) {
-      Swal.fire({
-        showConfirmButton: true,
-        icon: 'error',
-        text: 'La contraseña debe contener al menos: entre 8 y 16 caracteres, 1 número, 1 letra minúscula, 1 letra maypuscula y un carácter especial como #, @, %.'
-      })
-      return;
-    }
-
-    setIsLoading(true);
-    await axios.put(`https://localhost:4000/api/resetpassword/` + id + '/' + token, userPassword, {
-      where: {
-        id: id,
-        token: token
-      }
-    })
+    setIsLoading(true)
+    await axios
+      .put(
+        `https://localhost:4000/api/resetpassword/` + id + '/' + token,
+        userPassword,
+        {
+          where: {
+            id: id,
+            token: token,
+          },
+        }
+      )
       .then((res) => {
         setIsLoading(false)
         Swal.fire({
           showConfirmButton: true,
           icon: 'success',
-          text: 'Contraseña cambiada correctamente'
+          text: 'Contraseña cambiada correctamente',
         })
         navigate.push('/login')
-      }).catch((err) => {
+      })
+      .catch((err) => {
         Swal.fire({
           showConfirmButton: true,
           icon: 'error',
-          text: 'Ha habido un error al intentar enviar los datos, vuelva a intentarlo más tarde'
+          text: 'Ha habido un error al intentar enviar los datos, vuelva a intentarlo más tarde',
         })
-      });
+      })
   }
 
   const checkValidation = (e) => {
-    const confirmPass = e.target.value;
+    const confirmPass = e.target.value
     setConfirmPassword(confirmPass)
     if (password !== confirmPass) {
-      setIsError("La contraseña no coincide");
+      setIsError('La contraseña no coincide')
     } else {
-      setIsError("");
+      setIsError('')
     }
   }
 
   const switchShowPassword = () => {
-    setShowPassword(!showPassword);
+    setShowPassword(!showPassword)
   }
 
   return (
@@ -77,25 +73,38 @@ const ResetPassword = (props) => {
         <h3>Nueva contraseña</h3>
         <div className="divPassword"> Contraseña:*</div>
         <div className="containerPassword">
-          <input type={showPassword ? "text" : "password"} value={password} onChange={(e) => setPassword(e.target.value)} name="password" placeholder="Introduce tu contraseña" required />
-          <button onClick={switchShowPassword}>{showPassword ? "Ocultar" : "Mostrar"} </button>
+          <input
+            type={showPassword ? 'text' : 'password'}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            placeholder="Introduce tu contraseña"
+            required
+          />
+          <button onClick={switchShowPassword}>
+            {showPassword ? 'Ocultar' : 'Mostrar'}{' '}
+          </button>
         </div>
         <div> Confirmar contraseña:*</div>
-        <input type="password" value={confirmPassword} onChange={(e) => checkValidation(e)} name="confirmPassword" placeholder="Confirma la contraseña" required />
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => checkValidation(e)}
+          name="confirmPassword"
+          placeholder="Confirma la contraseña"
+          required
+        />
         <div className="confirmPassword">{isError}</div>
         <div className="divButton">
-          {isLoading
-            ?
-            <Spinner>
-              Cargando...
-            </Spinner>
-            :
+          {isLoading ? (
+            <Spinner>Cargando...</Spinner>
+          ) : (
             <button type="submit">Enviar</button>
-          }
+          )}
         </div>
       </form>
     </div>
   )
 }
 
-export { ResetPassword };
+export {ResetPassword}
