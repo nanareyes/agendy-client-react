8ad9b108fe9e50b4d96fe5820b45bf85d8e24295
nanareyes <dianac.reyesmantilla@gmail.com>
2021-11-24 20:26:58 -0500

    Se hacen pruebas en profile

--- src/Profile/controller.js
+++ src/Profile/controller.js
@@ -1,31 +1,29 @@
-import axios from 'axios';
-import { DateTime } from 'luxon';
+import axios from 'axios'
+import {DateTime} from 'luxon'
 // import { useNavigate } from "react-router-dom";
-import { useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { userState, fileState, loadingState } from '../atoms';
-
+import {useState} from 'react'
+import {useRecoilState} from 'recoil'
+import {userState, fileState, loadingState} from '../atoms'
 
 const useProfile = () => {
   // let navigate = useNavigate();
-  const [user, setUser] = useRecoilState(userState);
-  const [loading, setLoading] = useRecoilState(loadingState);
+  const [user, setUser] = useRecoilState(userState)
+  const [loading, setLoading] = useRecoilState(loadingState)
 
-  console.log("User en Profile controller", user);
+  console.log('User en Profile controller', user)
 
   const date = DateTime.fromISO(user.dateOfBirth)
-  const dateInput = date.toFormat("yyyy-mm-dd");
+  const dateInput = date.toFormat('yyyy-mm-dd')
 
-  const [inputDateOfBirth, setInputDateOfBirth] = useState(dateInput);
-  const [inputPhone, setInputPhone] = useState(user.phone);
-  const [inputAddress, setInputAddress] = useState(user.address);
-  const [inputCity, setInputCity] = useState(user.city);
-  const [inputUserType, setInputUserType] = useState(user.userType);
-  const [inputFile, setInputFile] = useRecoilState(fileState);
+  const [inputDateOfBirth, setInputDateOfBirth] = useState(dateInput)
+  const [inputPhone, setInputPhone] = useState(user.phone)
+  const [inputAddress, setInputAddress] = useState(user.address)
+  const [inputCity, setInputCity] = useState(user.city)
+  const [inputUserType, setInputUserType] = useState(user.userType)
+  const [inputFile, setInputFile] = useRecoilState(fileState)
 
-  const userName = user.loginType === 'GOOGLE' ? user.name : user.name;
-  const userId = user.loginType === 'GOOGLE' ? user.googleId : user._id;
-
+  const userName = user.name
+  const userId = user.loginType === 'GOOGLE' ? user.googleId : user._id
 
   const onChangeFile = (e) => {
     // const files = e.target.files;
@@ -40,22 +38,22 @@ const useProfile = () => {
     // } else {
     //   setInputFile(null);
     // }
-    console.info(e.target.files);
-    setInputFile(e.target.files[0]);
-  };
+    console.info(e.target.files)
+    setInputFile(e.target.files[0])
+  }
 
   const onSubmitProfile = (e) => {
     // console.log('Completar perfil', user);
-    e.preventDefault();
-
-    setLoading(true);
-    axios.put(`${process.env.REACT_APP_API_URL}/api/user/${user._id}`, {
-      dateOfBirth: inputDateOfBirth,
-      phone: inputPhone,
-      address: inputAddress,
-      city: inputCity,
-      userType: inputUserType,
-    })
+    e.preventDefault()
+    setLoading(true)
+    axios
+      .put(`${process.env.REACT_APP_API_URL}/api/user/${user._id}`, {
+        dateOfBirth: inputDateOfBirth,
+        phone: inputPhone,
+        address: inputAddress,
+        city: inputCity,
+        userType: inputUserType,
+      })
       .then(function (response) {
         console.log('Actualización exitosa')
         // Una vez se actualice el perfil, se procede a actualizar el
@@ -63,24 +61,29 @@ const useProfile = () => {
         // que hayan actualmente en el usuario (currentUser) y se le adicionan
         // los datos que retorne el API
 
-        console.info("inputFile", inputFile);
-        const formData = new FormData();
-        formData.append('file', inputFile);
+        console.info('inputFile', inputFile)
+        const formData = new FormData()
+        formData.append('file', inputFile)
         const config = {
-            headers: {
-                'content-type': 'multipart/form-data'
-            }
-        };
-        axios.post(`${process.env.REACT_APP_API_URL}/api/profile/${user._id}`, formData, config)
+          headers: {
+            'content-type': 'multipart/form-data',
+          },
+        }
+        axios
+          .post(
+            `${process.env.REACT_APP_API_URL}/api/profile/${user._id}`,
+            formData,
+            config
+          )
           .then(function (response) {
             setUser((currentUser) => {
               const newUser = {
                 ...currentUser,
-                ...response.data
-              };
+                ...response.data,
+              }
               // console.log("newUSer en profile", newUser);
-              return newUser;
-            });
+              return newUser
+            })
           })
         //navigate("/home");
       })
@@ -88,9 +91,10 @@ const useProfile = () => {
         // console.log(error);
         console.log('Usuario no actualizado')
         alert('Error, la actualización no fue exitosa')
-      }).finally(function() {
-        setLoading(false);
-      });
+      })
+      .finally(function () {
+        setLoading(false)
+      })
   }
 
   return {
@@ -109,8 +113,8 @@ const useProfile = () => {
     onSubmitProfile,
     onChangeFile,
     inputFile,
-    loading
+    loading,
   }
-};
+}
 
-export { useProfile };
+export {useProfile}
