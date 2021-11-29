8e9f7dcd1ea5fb818f9df60fe0483965d86628fe
nanareyes <dianac.reyesmantilla@gmail.com>
2021-11-29 12:24:11 -0500

    Se integra la sección de galería y servicios, se cambia la ruta para importar componentes

--- src/Gallery/CardsGallery.js
+++ src/Gallery/CardsGallery.js
@@ -1,8 +1,7 @@
-import React from "react";
-import CardGallery from "./CardGallery";
+import React from 'react'
+import CardGallery from './CardGallery'
 
-
-import image1 from "../assets/imagesgallery/decorado1.jpg";
+import image1 from '../assets/imagesgallery/decorado1.jpg'
 import image2 from '../assets/imagesgallery/decorado5.jpg'
 import image3 from '../assets/imagesgallery/decorado7.jpg'
 import image4 from '../assets/imagesgallery/decorado14.jpg'
@@ -13,95 +12,83 @@ import image8 from '../assets/imagesgallery/decorado22.jpg'
 import image9 from '../assets/imagesgallery/decorado21.jpg'
 import image10 from '../assets/imagesgallery/decorado12.jpg'
 import image11 from '../assets/imagesgallery/decorado8.jpg'
-import image12 from '../assets/imagesgallery/decorado3.jpg' 
+import image12 from '../assets/imagesgallery/decorado3.jpg'
 
 const cardsGallery = [
   {
     id: 1,
-    title: "Olas del Mar",
+    title: 'Olas del Mar',
     image: image1,
-    
   },
   {
     id: 2,
-    title: "Flor Blanca",
+    title: 'Flor Blanca',
     image: image2,
-    
   },
   {
     id: 3,
-    title: "Corazón",
+    title: 'Corazón',
     image: image3,
-    
   },
   {
     id: 4,
-    title: "Tropical",
+    title: 'Tropical',
     image: image4,
-    
   },
   {
     id: 5,
-    title: "Animal Print",
+    title: 'Animal Print',
     image: image5,
-    
   },
   {
     id: 6,
-    title: "Onda Rosa",
+    title: 'Onda Rosa',
     image: image6,
-    
   },
   {
     id: 7,
-    title: "Animal print 2",
+    title: 'Animal print 2',
     image: image7,
-    
   },
   {
     id: 8,
-    title: "Morado- Pink",
+    title: 'Morado- Pink',
     image: image8,
-    
   },
   {
     id: 9,
-    title: "Negro_plate",
+    title: 'Negro_plate',
     image: image9,
-    
   },
   {
     id: 10,
-    title: "Onda Rosa",
+    title: 'Onda Rosa',
     image: image10,
-    
   },
   {
     id: 11,
-    title: "Mixto",
+    title: 'Mixto',
     image: image11,
-    
   },
   {
     id: 12,
-    title: "Mariposas",
+    title: 'Mariposas',
     image: image12,
-    
   },
-]; 
+]
 
 function CardsGallery() {
   return (
     <div className="container d-flex justify-content-center align-items-center h-100">
       <div className="row">
-        {cardsGallery.map(({ title, image, id }) => (
+        {cardsGallery.map(({title, image, id}) => (
           <div className="col-md-4" key={id}>
             <CardGallery imageSource={image} title={title} />
           </div>
         ))}
       </div>
     </div>
-  );
+  )
 }
 
-export default CardsGallery;
+export default CardsGallery
--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,46 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import Pagination from './Services/Pagination';
-import axios from 'axios';
-import './Services.css';
-import CardsView from './Services/Cards';
+import React, {useState, useEffect} from 'react'
+import Pagination from './Pagination'
+import axios from 'axios'
+import './Services.css'
+import CardsView from './Cards'
 
 const Services = () => {
-  const [Cards, setCards] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [cardsPerPage] = useState(4);
+  const [Cards, setCards] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [cardsPerPage] = useState(4)
 
   useEffect(() => {
     const fetchCards = async () => {
-      setLoading(true);
-      const res = await axios.get('http://localhost:3000');
-      setCards(res.data);
-      setLoading(false);
-    };
+      setLoading(true)
+      const res = await axios.get('http://localhost:3000')
+      setCards(res.data)
+      setLoading(false)
+    }
 
-    fetchCards();
-  }, []);
+    fetchCards()
+  }, [])
 
   // Get current cards
-  const indexOfLastCard = currentPage * cardsPerPage;
-  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
-  const currentCards = Cards.slice(indexOfFirstCard, indexOfLastCard);
+  const indexOfLastCard = currentPage * cardsPerPage
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage
+  const currentCards = Cards.slice(indexOfFirstCard, indexOfLastCard)
 
   // Change page
-  const paginate = pageNumber => setCurrentPage(pageNumber);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
 
   return (
-    <div className='container mt-5'>
-      <h3 className='text-primary mb-1'>MANOS Y PIES</h3>
-      <CardsView posts={currentCards} loading={loading} /> 
+    <div className="container mt-5">
+      <h3 className="text-primary mb-1">MANOS Y PIES</h3>
+      <CardsView posts={currentCards} loading={loading} />
       <Pagination
         cardsPerPage={cardsPerPage}
         totalcards={12}
-        paginate={paginate} 
-      /> 
-      
+        paginate={paginate}
+      />
     </div>
-  );
-};
+  )
+}
 
-export default Services;
+export default Services
